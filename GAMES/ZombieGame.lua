409f03a75042811e6dabd8e16b073153804346d6
NoobHubV7 <phamtrinh.12331@gmail.com>
2026-08-29 09:13:35 +0700

    Update yield parameter in Loader function

--- GAMES/ZombieGame.lua
+++ GAMES/ZombieGame.lua
@@ -1,6 +1,6 @@
 local Compkiller = loadstring(game:HttpGet("https://raw.githubusercontent.com/4lpaca-pin/CompKiller/refs/heads/main/src/source.luau"))();
 
-Compkiller:Loader("rbxassetid://15544034532", 6).yield()
+Compkiller:Loader("rbxassetid://15544034532", 10).yield()
 
 local FileWatcher = Compkiller:ConfigManager({
 	Directory = "Compkiller",
