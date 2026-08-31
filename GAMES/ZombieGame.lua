f70500351e506feae78e7334475ee9fe15f7e9e5
NoobHubV7 <phamtrinh.12331@gmail.com>
2026-08-31 11:08:11 +0700

    Fix HumanNpc handling in ZombieGame logic

--- GAMES/ZombieGame.lua
+++ GAMES/ZombieGame.lua
@@ -783,7 +783,6 @@ task.spawn(function()
 				    if (v and v:IsA("Model")) and v.FindFirstChild(v, "Humanoid").Health > 0 and not v:FindFirstChildOfClass("ForceField") then
 				    	if v ~= game.Players.LocalPlayer.Character and v:FindFirstChild("HumanoidRootPart") and ((game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude - (game.Players.LocalPlayer.Character.HumanoidRootPart.Size.Magnitude / 2) - (v.HumanoidRootPart.Size.Magnitude / 2)) <= 7 then
 				    		if game.Players.LocalPlayer.Character:GetAttribute("Team") ~= "Zombie" then
-				    			if v.Name == "HumanNpc" then continue end
 				    			if (string.find(v.Name, "Zombie") or string.find(v.Name, "Summon") or string.find(v.Name, "Necro")) then
 					    			local weapon = find.Weapons(true) or find.Weapons(false)
 							    	if not weapon and (game.Players.LocalPlayer.Character:GetAttribute("Team") == "Human") then
@@ -801,6 +800,7 @@ task.spawn(function()
 									    end
 								    end)
 						    	end
+							    if v.Name == "HumanNpc" then return end
 						    end
 					    end
 				    end
